dfce171888c86423bc6709bb1fb6c80bdc017078
nadanote <jaewon0002@gmail.com>
2025-01-07 13:46:28 +0900

    네비게이션바 정렬, 퀴즈/학습 페이지 크기 균일화

--- mainpage.php
+++ mainpage.php
@@ -15,66 +15,66 @@
 <body>
   <?php include 'header.php'; ?>
   <main>
-    <div class="testbox">
-      <div class="testcard-1">
-        <div class="card" style="width: 18rem">
-          <img src="./img/169.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">인간 관련 수어 맞추기</h5>
-            <p class="card-text">
-              한국수어 사전에 기재된 인간 관련 일상생활 수어 20문제입니다.
-            </p>
-            <a href="./people.php" class="btn btn-success">공부하기</a>
-            <a href="./quiz_people.php" class="btn btn-primary">문제 풀러가기</a>
+      <div class="testbox">
+        <div class="testcard-1">
+          <div class="card" style="width: 18rem">
+            <img src="./img/169.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">인간 관련 수어 맞추기</h5>
+              <p class="card-text">
+                한국수어 사전에 기재된 인간 관련 일상생활 수어 20문제입니다.
+              </p>
+              <a href="./people.php" class="btn btn-success">공부하기</a>
+              <a href="./quiz_people.php" class="btn btn-primary">문제 풀러가기</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="testcard-2">
+          <div class="card" style="width: 18rem">
+            <img src="./img/169.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">삶 관련 수어 맞추기</h5>
+              <p class="card-text">
+                한국수어 사전에 기재된 삶 관련 일상생활 수어 20문제입니다.
+              </p>
+              <a href="./life.php" class="btn btn-success">공부하기</a>
+              <a href="./quiz_life.php" class="btn btn-primary">문제 풀러가기</a>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="testcard-2">
-        <div class="card" style="width: 18rem">
-          <img src="./img/169.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">삶 관련 수어 맞추기</h5>
-            <p class="card-text">
-              한국수어 사전에 기재된 삶 관련 일상생활 수어 20문제입니다.
-            </p>
-            <a href="./life.php" class="btn btn-success">공부하기</a>
-            <a href="./quiz_life.php" class="btn btn-primary">문제 풀러가기</a>
+      <div class="testbox">
+        <div class="testbox-3">
+          <div class="card" style="width: 18rem">
+            <img src="./img/169.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">식생활 관련 수어 맞추기</h5>
+              <p class="card-text">
+                한국수어 사전에 기재된 식생활 관련 일상생활 수어 20문제입니다.
+              </p>
+              <a href="./foodLife.php" class="btn btn-success">공부하기</a>
+              <a href="./quiz_food.php" class="btn btn-primary">문제 풀러가기</a>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <div class="testbox">
-      <div class="testbox-3">
-        <div class="card" style="width: 18rem">
-          <img src="./img/169.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">식생활 관련 수어 맞추기</h5>
-            <p class="card-text">
-              한국수어 사전에 기재된 식생활 관련 일상생활 수어 20문제입니다.
-            </p>
-            <a href="./foodLife.php" class="btn btn-success">공부하기</a>
-            <a href="./quiz_food.php" class="btn btn-primary">문제 풀러가기</a>
+        <div class="testbox-4">
+          <div class="card" style="width: 18rem">
+            <img src="./img/169.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">의생활 관련 수어 맞추기</h5>
+              <p class="card-text">
+                한국수어 사전에 기재된 의생활 관련 일상생활 수어 20문제입니다.
+              </p>
+              <a href="./clothingLife.php" class="btn btn-success">공부하기</a>
+              <a href="./quiz_clothing.php" class="btn btn-primary">문제 풀러가기</a>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="testbox-4">
-        <div class="card" style="width: 18rem">
-          <img src="./img/169.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">의생활 관련 수어 맞추기</h5>
-            <p class="card-text">
-              한국수어 사전에 기재된 의생활 관련 일상생활 수어 20문제입니다.
-            </p>
-            <a href="./clothingLife.php" class="btn btn-success">공부하기</a>
-            <a href="./quiz_clothing.php" class="btn btn-primary">문제 풀러가기</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+    </main>
   <footer>
     <b>Copyright &copy; 수얼 Co.Ltd. All rights reserved.</b>
   </footer>
